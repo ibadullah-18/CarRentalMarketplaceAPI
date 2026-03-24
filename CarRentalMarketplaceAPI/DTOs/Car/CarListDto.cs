4316d2ae1c1b3ba8da6551a4c82553dc4e794dae
ibadullah-18 <Husey_iz52@itstep.edu.az>
2026-03-24 12:04:52 +0400

    her sey ela

--- CarRentalMarketplaceAPI/DTOs/Car/CarListDto.cs
+++ CarRentalMarketplaceAPI/DTOs/Car/CarListDto.cs
@@ -5,6 +5,12 @@ public class CarListDto
     public Guid Id { get; set; }
     public string Brand { get; set; } = string.Empty;
     public string Model { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string Color { get; set; } = string.Empty;
+    public int Mileage { get; set; }
+    public string FuelType { get; set; } = string.Empty;
+    public string Transmission { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public decimal PricePerDay { get; set; }        
     public string MainImageUrl { get; set; } 
     public string Location { get; set; } = string.Empty;
